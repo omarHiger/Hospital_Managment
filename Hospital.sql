abda293a4eeca2e09b95ea99a304f5814d1527bc
Omar <ohiger2015@gmail.com>
2021-12-07 14:26:07 +0300

    Edited sql file

--- Hospital.sql
+++ Hospital.sql
@@ -1,19 +1,22 @@
 create table hospital(
 
-SerialNumber int primary key,
+SerialNumber int,
 Name_HO nvarchar(50),
 City nvarchar(50),
 Street nvarchar(50),
-Area nvarchar(50)
+Area nvarchar(50),
+Mgr_Id int,
+constraint HO_SerialNumber_PK primary key (SerialNumber),
+constraint hospital_Mgr_Id_FK foreign key (Mgr_Id) references Doctor(Do_Id)
 )
 create table HO_Phone(
-  HO_Phone nvarhcar(10),
-  Ho_SerialNumber ,
-  constraint Ho_Phone_SerialNumber_FK foreign key (Ho_SerialNumber) refernces  hospital(SerialNumber)
+  HO_Phone nvarchar(10),
+  Ho_SerialNumber int ,
+  constraint Ho_Phone_SerialNumber_FK foreign key (Ho_SerialNumber) references  hospital(SerialNumber)
 )
 create table Doctor
 (
-Doc_ID int primary key,
+Do_ID int,
 First_Name nvarchar(50),
 Middle_Name nvarchar(50),
 Last_Name  nvarchar(50),
@@ -29,16 +32,31 @@ Area nvarchar(50),
 social_status nvarchar(50),
 Family_mamber  int ,
 Specialization nvarchar(50),
-Qualificationsþ nvarchar(50),
-Ho_SerialNumber int ,  
-Manager_ID int ,  
-constraint Do_Hospital_SerialNumber_FK foreign key (Ho_SerialNumber) refernces  hospital(SerialNumber),
-constraint Do_Manager_ID_FK foreign key (Manager_ID) refernces  doctor(Doc_ID)
+Qualificationsþ nvarchar(50),  
+Manager_ID int ,
+constraint DO_ID_PK primary key (Do_ID),
+constraint Do_Manager_ID_FK foreign key (Manager_ID) references  doctor(Do_ID)
 )
 
+create table Doctor_Phone_number
+(
+	Do_Phone nvarchar(10),
+	Do_Id int,
+	constraint Doctor_Phone_number_PK primary key (Do_Phone,Do_Id),
+	constraint Doctor_Phone_number_DO_ID_FK foreign key (DO_Id) references  doctor(Do_ID)
+)
+
+create table Do_Work_HO
+(
+	Do_Id int,
+	SerialNumber int,
+	constraint Do_Work_HO_PK primary key (SerialNumber,Do_Id),
+	constraint Do_Work_HO_DO_ID_FK foreign key (DO_Id) references  doctor(Do_ID),
+	constraint Do_Work_HO_SerialNumber_FK foreign key (SerialNumber) references  hospital(SerialNumber)
+)
 create table Patient
 (
-Pa_ID int primary,
+Pa_ID int,
 First_Name nvarchar(50),
 Middle_Name nvarchar(50),
 Last_Name  nvarchar(50),
@@ -54,31 +72,116 @@ National_number int unique ,
 social_status nvarchar(50),
 Career nvarchar(50),
 Birth_Place nvarchar(max),
+Ho_SerialNumber int,
+constraint Pa_ID_PK primary key (Pa_ID),
+constraint Patient_HO_SerialNumber_FK foreign key (Ho_SerialNumber) references  hospital(SerialNumber)
 )
 
-create table Patient_Phone_numPa
+create table Caregiver
 (
-Phone_numPa int primary key
-ID_Pa int 
+	Do_Id int,
+	Pa_Id int,
+	constraint Caregiver_PK primary key (Pa_Id,Do_Id),
+	constraint Caregiver_DO_ID_FK foreign key (DO_Id) references  doctor(Do_ID),
+	constraint Caregiver_Pa_ID_FK foreign key (Pa_Id) references  Patient(Pa_ID),
 )
 
+
+create table Patient_Phone_number
+(
+	Pa_Phone nvarchar(10),
+	Pa_Id int,
+	constraint Patient_Phone_number_PK primary key (Pa_Phone,Pa_Id),
+	constraint Patient_Phone_number_Pa_ID_FK foreign key (Pa_Id) references  Patient(Pa_ID) 
+)
+
+create table Previews
+(
+	Pre_Id int identity(1,1),
+	Previews_Date smalldatetime,
+	DO_Id int ,
+	Pa_ID int ,
+	constraint Previews_PK primary key (Pre_Id),
+	constraint Previews_DO_ID_FK foreign key (DO_Id) references  doctor(Do_ID),
+	constraint Previews_Pa_ID_FK foreign key (Pa_Id) references  Patient(Pa_ID),
+
+	
+)
+
+create table Bills
+(
+	Bill_Id int,
+	Examinations money,
+	Surgeries money,
+	Room_Service money,
+	Medical_tests money,
+	Rays money,
+	constraint Bills_PK primary key (Bill_Id),
+)
+
+create table Payments
+(
+	Pa_Id int,
+	Bill_Id int,
+	constraint Payments_PK primary key (Pa_Id,Bill_Id),
+	constraint Payments_Bill_ID_FK foreign key (Bill_Id) references  Bills(Bill_Id),
+	constraint Payments_Pa_ID_FK foreign key (Pa_Id) references  Patient(Pa_ID),
+)
+
+create table Medical_Details_External_Records
+(
+	External_Records nvarchar(max),
+	constraint Medical_Details_Examenation_Records_PK primary key (External_Records)
+)
 
 create table Medical_Details
 (
-ID_Pa int ,
+Pa_Id int,
 Examenation_Records nvarchar(max),
 Blood_type nvarchar(5),
-Treatment plans&Daily supplements nvarchar(max)
-Allergies nvarchar(50),
-Examenation_Records nvarchar(50),
-Family_Health_History nvarchar(50),
-Special_Needs nvarchar(50),
-Chronic_diseases nvarchar
+Treatment_plans_Daily_supplements nvarchar(max),
+Allergies nvarchar(max),
+Family_Health_History nvarchar(max),
+Special_Needs nvarchar(max),
+Chronic_diseases nvarchar(max),
+External_Records nvarchar(max),
+constraint Medical_Details_Examenation_Records_FK foreign key (Examenation_Records) references  Medical_Details_External_Records(External_Records),
+constraint Previews_Pa_ID_FK foreign key (Pa_Id) references  Patient(Pa_ID),
+)
+
+create table Medical_tests
+(
+	Test_Id int,
+	Pa_Id int,
+	Test_Type nvarchar(50),
+	Test_Result nvarchar(50),
+	constraint Medical_tests_PK primary key (Test_Id),
+	constraint Medical_tests_Pa_ID_FK foreign key (Pa_Id) references  Patient(Pa_ID),
+)
+
+create table Rays
+(
+	Rays_Id int,
+	Pa_Id int,
+	Rays_Type nvarchar(50),
+	Rays_Result nvarchar(50),
+	constraint Rays_PK primary key (Rays_Id),
+	constraint Rays_Pa_ID_FK foreign key (Pa_Id) references  Patient(Pa_ID),
+)
+
+
+create table Department
+(
+Dept_ID int,
+Dept_name nvarchar(50),
+Ho_SerialNumber int ,
+constraint Department_PK primary key (Dept_ID),
+constraint Department_HO_SerialNumber_FK foreign key (Ho_SerialNumber) references  hospital(SerialNumber),
 )
 
 create table Employee
 (
-ID_emp int primary key not null,
+Emp_Id int,
 First_Name nvarchar(50),
 Middle_Name nvarchar(50),
 Last_Name  nvarchar(50),
@@ -93,44 +196,83 @@ Acount nvarchar(50),
 Emp_Type nvarchar(50),
 Qualificationsþ nvarchar(max),
 social_status nvarchar(50),
-Family_mamber  int 
+Family_member  int,
+Dept_Id int,
+constraint Employee_PK primary key (Emp_Id),
+constraint Employee_Dept_ID_FK foreign key (Dept_Id) references  Department(Dept_ID),
+)
+
+create table Emp_Phone_number
+(
+	Emp_Phone nvarchar(10),
+	Emp_Id int,
+	constraint Emp_Phone_number_PK primary key (Emp_Phone,Emp_Id),
+	constraint Emp_Phone_number_Emp_ID_FK foreign key (Emp_Id) references  Employee(Emp_Id) 
 )
 
 
-create table Department
+create table Do_Workin_Dept -- we'll think about this again!!!
 (
-Dept_ID int primary key not null,
-Dept_name nvarchar(50),
-Dept_type nvarchar(50)
+	Dept_Id int,
+	Do_Id int,
+	constraint Do_Workin_Dept_PK primary key (Dept_ID,Do_Id),
+	constraint Do_Workin_Dept_DO_ID_FK foreign key (DO_Id) references  doctor(Do_ID),
+	constraint Do_Workin_Dept_Dept_Id_FK foreign key (Dept_Id) references  Patient(Pa_ID),
 )
 
 create table Room
 (
-Room_ID int primary key not null,
+Room_ID int,
 Room_type nvarchar(20),
-Empity_or_not bit 
+Empity_or_not bit ,
+constraint Room_Room_Id_PK primary key (Room_Id),
 )
 
+create table Room_Reservation
+(
+--الكيان الضعيف هل يأخذ مفتاح أساسي؟؟
+	Pa_Id int ,
+	Room_Id int,
+	Reservation_Date smalldatetime,
+	Reservation_EndDate smalldatetime,
+	constraint Room_Reservation_Pa_Id_FK foreign key (Pa_Id) references  Patient(Pa_ID),
+	constraint Room_Reservation_Room_Id_FK foreign key (Room_Id) references  Room(Room_ID),
+
+)
 create table Surgery_Room
 (
-Room_ID_SU int primary key not null, 
-Ready_or_not bit
-Serial_number_HO int 
+
+Surgery_Room_ID int, 
+Ready_or_not bit,
+HO_Serial_number int,
+constraint Surgery_Room_PK primary key (Surgery_Room_ID),
+constraint Surgery_Room_HO_SerialNumber_FK foreign key (HO_Serial_number) references  hospital(SerialNumber)
 )
 
 create table Surgery
 (
-Surgery_number int primary key not null,
-Surgery_name nvarchar(50),
-Surgery_date date,
-Pa_ID int,
-Doc_ID int 
+	Surgery_number int,
+	Surgery_name nvarchar(50),
+	Surgery_date smalldatetime,
+	Surgery_Room_ID int,
+	Pa_ID int,
+	Do_ID int,
+	constraint Surgery_PK primary key (Surgery_number),
+	constraint Surgery_DO_ID_FK foreign key (DO_Id) references  doctor(Do_ID),
+	constraint Surgery_Dept_Id_FK foreign key (Pa_Id) references  Patient(Pa_ID),
+	constraint Surgery_Surgery_Room_ID_FK foreign key (Surgery_Room_ID) references  Surgery_Room(Surgery_Room_ID),
 )
 
 create table Death_Cases
 (
-ID_Pa int ,
+Pa_Id int unique,
 Death_date date,
-Cause_death nvarchar(50)
+Cause_death nvarchar(1000),
+constraint Death_Cases_Pa_ID_FK foreign key (Pa_Id) references  Patient(Pa_ID),
 )
 
+create table [Admin]
+(
+	Email nvarchar(50),
+	Password nvarchar(50)
+)
